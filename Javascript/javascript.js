230051ef0d4f5b89d70564b279e623107fcbd7c3
Roman <romanshliakhov@gmail.com>
2022-03-19 12:00:04 +0200

    finished 20 exercises

---
+++ Javascript/javascript.js
@@ -0,0 +1,48 @@
+// Basic JavaScript
+
+// Ex 1. Comment Your JavaScript Code
+
+// My comment
+/* This is my
+multi-line comment */
+
+// Ex 2. Declare JavaScript Variables
+
+// var myName;
+
+// Ex 3. Storing Values with the Assignment Operator
+
+// Ex 4. Assigning the Value of One Variable to Another
+
+// Ex 5. SInitializing Variables with the Assignment Operator
+
+// Ex 6. Declare String Variables
+
+// Ex 7. Understanding Uninitialized Variables
+
+// Ex 8. Understanding Case Sensitivity in Variables
+
+// Ex 9. Explore Differences Between the var and let Keywords
+
+// Ex 10. Declare a Read-Only Variable with the const Keyword
+
+// Ex 11. Add Two Numbers with JavaScript
+
+// Ex 12. Subtract One Number from Another with JavaScript
+
+// Ex 13. Multiply Two Numbers with JavaScript
+
+// Ex 14. Divide One Number by Another with JavaScript
+
+// Ex 15. Increment a Number with JavaScript
+
+// Ex 16. Decrement a Number with JavaScript
+
+// Ex 17. Create Decimal Numbers with JavaScript
+
+// Ex 18. Multiply Two Decimals with JavaScript
+
+// Ex 19. Divide One Decimal by Another with JavaScript
+
+// Ex 20. Finding a Remainder in JavaScript
+
