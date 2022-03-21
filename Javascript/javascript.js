bd210745000035f977dd2c8ddeaa3b8a4ac4c16b
Roman <romanshliakhov@gmail.com>
2022-03-21 16:44:42 +0200

    added 21-25 JS exercises

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -46,3 +46,45 @@ multi-line comment */
 
 // Ex 20. Finding a Remainder in JavaScript
 
+// Ex 21. Compound Assignment With Augmented Addition
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// // Only change code below this line
+// a += 12;
+// b += 9;
+// c += 7;
+
+// Ex 22. Compound Assignment With Augmented Subtraction
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// // Only change code below this line
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+// Ex 23. Compound Assignment With Augmented Multiplication
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+
+// // Only change code below this line
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// Ex 24. Compound Assignment With Augmented Division
+// let a = 48;
+// let b = 108;
+// let c = 33;
+
+// // Only change code below this line
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
+// Ex 25. Escaping Literal Quotes in Strings
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
